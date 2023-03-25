b3c0c7afa423e2fa3ea4ffd646dadb043c631db2
B O S C H <116667373+bosch0@users.noreply.github.com>
2023-03-25 12:09:11 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -5,4 +5,4 @@ HUD.framework = 'QB-Core' -- QB-Core / ESX
 HUD.health = 50 -- Determina a que % de vida se muestra el health-bar
 HUD.armour = 0 -- Determina a que % de vida se muestra el armour-bar
 HUD.hunger = 50 -- Determina a que % de vida se muestra el hunger-bar
-HUD.thirst = 50 -- Determina a que % de vida se muestra el thrist-bar
+HUD.thirst = 50 -- Determina a que % de vida se muestra el thirst-bar
